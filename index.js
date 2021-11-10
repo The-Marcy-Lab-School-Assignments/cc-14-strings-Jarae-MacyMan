aa2ff98e7036bb160ec80ca1274f074b523f43da
Jarae-MacyMan <sjaraebr@gmail.com>
2021-11-10 15:55:23 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//write your code here
+let strLowerCase = (str) => {
+let newStr = "";
+for (let i = 0; i < str.length; i++){
+  if (str[i] === str[i].toLowerCase()){
+     newStr += str[i];
+  }
+}
+ return newStr;
+};
+
+console.log(strLowerCase("Yaya+Jarae!!BBHH"));//write your code here
